af57797e5d31292acb56f68e42443f4209ef42e0
samro <samro@host.docker.internal>
2026-04-14 15:11:07 +0530

    adding api packages...

---
+++ src/main/java/com/api/endpoints/Routes.java
@@ -0,0 +1,11 @@
+package com.api.endpoints;
+
+public class Routes {
+	// usr endPoints
+	public static String baseUrl = "https://petstore.swagger.io/v2";
+	public static String postUrl = baseUrl +"/user";
+	public static String getUrl = baseUrl + "/user/{username}";
+	public static String putUrl = baseUrl + "/user/{username}";
+	
+
+}
---
+++ src/main/java/com/api/endpoints/UserEndpoints.java
@@ -0,0 +1,19 @@
+package com.api.endpoints;
+
+import static io.restassured.RestAssured.given;
+
+
+
+public class UserEndpoints {
+	
+	
+	public static Response createUser() {
+		Response res =given()
+		
+		.when()
+		
+		return res;
+	}
+	
+
+}
